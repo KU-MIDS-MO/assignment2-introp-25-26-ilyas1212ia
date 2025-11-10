2c51c7a45d120b6cd9f25de0c33286105c060e2f
Ilyas Hamada <ilyashamada@Ilyass-MacBook-Air.local>
2025-11-10 21:13:37 +0100

    done

--- breakdown_time.py
+++ breakdown_time.py
@@ -1,4 +1,23 @@
 def breakdown_time(seconds):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+
+    if type(seconds) != int or seconds < 0:
+        return -1
+    if seconds == 0:
+        return {}
+    
+    time_dict = {}
+    
+    hours = seconds // 3600 
+    if hours > 0:
+        time_dict[3600] = hours
+    seconds = seconds % 3600
+    
+    minutes = seconds // 60
+    if minutes > 0:
+        time_dict[60]= minutes
+    seconds = seconds % 60
+    
+    if seconds > 0:
+        time_dict[1] = seconds
+        
+    return time_dict
--- swap_ends.py
+++ swap_ends.py
@@ -1,4 +1,12 @@
 def swap_ends(L, k):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+
+    if k <= 0 or len(L) == 0 or k > len(L)//2:
+        return (L.copy(), 0)
+    new_list = L.copy()
+    first_part = L[:k]
+    last_part = L[-k:]
+    new_list[:k] = last_part
+    new_list[-k:] = first_part
+    return (new_list, k)
+
+print(swap_ends([1, 2, 3, 4, 5, 6], 2))
